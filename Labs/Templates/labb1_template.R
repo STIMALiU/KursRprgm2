7ce009cc821d8c0577273c3d5d5eb0c0c109d1e1
Johan Alenlöv <johan@alenlov.se>
2022-01-26 10:56:53 +0100

    Labb1 exempel

---
+++ Labs/Templates/labb1_template.R
@@ -0,0 +1,56 @@
+#### Programmering i R ####
+#### Datorlaboration1 ####
+
+# spara denna fil enligt:
+# del 1: labb1_josad732.R om ditt liu-ID är josad732 och det är labb 1 .
+# del 2: labb5_grupp01.R om ni tillhör grupp 01 och det är labb 5.
+
+
+# Skiv ditt namn här, ex Namn <- "Alice Walker"
+# På del 2 i kursen, när ni arbetar i par, skriv då namn på formen:  
+# Namn <- c("Ada Lovelace", "Alice Walker")
+Namn <- "Johan Alenlöv"
+
+
+# Skiv ditt liu ID här, ex LiuId <- "aliwa123"
+# På del 2 i kursen, när ni arbetar i par, skriv då ID på formen:  
+# LiuId <- c("adalo456", "aliwa123")
+LiuId  <- "johal950"
+
+# På del 2 i kursen, när ni arbetar i par, ange även grupptillhörighet enligt: 
+# Grupp<-"grupp01", om ni tillhör grupp01
+# Grupp<-""
+
+
+
+# return_first_last
+return_first_last <- function(){ # OBS argument saknas
+  # Kod här
+}
+
+# orth_scalar_prod
+orth_scalar_prod <- function(){ # OBS argument saknas
+  # Kod här
+}
+
+# hi_name
+hi_name <- function(){ # OBS argument saknas
+  # Kod här
+}
+
+# approx_e
+approx_e <- function(){ # OBS argument saknas
+  # Kod här
+}
+
+# OBS: kommentera bort all kod som inte är de obligatoriska variablerna ovan 
+# eller funktionerna som defineras i inlämningsuppgifterna!
+
+
+# Använda markmyassignment
+library(markmyassignment)
+set_assignment("https://raw.githubusercontent.com/STIMALiU/KursRprgm2/main/Labs/Tests/d1.yml")
+show_tasks()
+mark_my_assignment()
+# mark_my_file()
+# OBS: kommentera bort markmyassignment-kod innan laboration lämnas in.
