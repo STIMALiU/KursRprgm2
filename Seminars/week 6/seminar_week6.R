fb1b1c53874db804719a5f531bbda0077243ce25
josefwilzen <josefwilzen@gmail.com>
2023-03-02 14:42:03 +0100

    updates

--- Seminars/week 6/seminar_week6.R
+++ Seminars/week 6/seminar_week6.R
@@ -24,7 +24,7 @@ B
 
 matrix(data = 0,ncol = 3,nrow = 3)
 C<-matrix(0,3,3)
-C
+#C<-diag(c(1,0,0))
 
 # D är en diagonalmatris
 D<-diag(x = c(1,2,3))
@@ -43,8 +43,47 @@ solve(X1_temp)
 
 X1<-solve(A-B%*%solve(D)%*%C)
 
-X1
+
 #
+X2<- -solve(A-B%*%solve(D)%*%C)%*%B%*%solve(D)
+X2
+
+# 
+X3<- -solve(D)%*%C%*%solve(A-B%*%solve(D)%*%C)
+
+X4<- solve(D)+solve(D)%*%C%*%(-X2)
 
 
+cbind(X1,X2)
+cbind(X3,X4)
+
+X<-rbind(cbind(X1,X2),cbind(X3,X4))
+
+# uppgift 10
+
+Y<-solve( rbind( cbind(A,B) , cbind(C,D) ) )
+
+# testa att addera lite fel till Y
+#Y<-Y+1e-6
+
+X
+Y
+
+# uppgift 11: kolla om X och Y är samma
+
+a<-2
+b<-2
+
+a==b
+
+a-b
+
+X==Y
+all(X==Y)
+
+X-Y
+abs(X-Y)
+tol_val<-1e-8
+abs(X-Y)<tol_val
+all(abs(X-Y)<tol_val)
 
