d114d4b1cf8906d6ab03f34455edb7689ee68428
josefwilzen <josefwilzen@gmail.com>
2023-03-02 13:39:49 +0100

    updates

--- Seminars/week 6/seminar_week6.R
+++ Seminars/week 6/seminar_week6.R
@@ -11,3 +11,40 @@
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
+# Linjär algebra: 2.2
+# uppgift 8
+
+A <- matrix(c(1,1,1,1,1,2,1,3,4),ncol=3)
+B <- matrix(c(-1,2,2,-2,-2,1,1,1,-1),ncol=3)
+
+A
+B
+# skapa C och D enligt uppgiften
+# C har bara nollor
+
+matrix(data = 0,ncol = 3,nrow = 3)
+C<-matrix(0,3,3)
+C
+
+# D är en diagonalmatris
+D<-diag(x = c(1,2,3))
+D
+
+# uppgift 9
+
+# vi ska göra en blockmatris med fyra block
+# vi kallar dem X1, X2, X3 och X4
+
+# X1
+# X1 = (A-B(D^-1)C)^-1
+
+X1_temp<-A-B%*%solve(D)%*%C
+solve(X1_temp)
+
+X1<-solve(A-B%*%solve(D)%*%C)
+
+X1
+#
+
+
+
