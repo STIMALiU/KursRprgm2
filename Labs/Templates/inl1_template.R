e8da348e3b5efb26fc1031831358cee56ce7b274
Johan Alenlöv <johan@alenlov.se>
2024-04-02 11:59:21 +0200

    Ny mall

---
+++ Labs/Templates/inl1_template.R
@@ -0,0 +1,41 @@
+#### Programmering i R ####
+#### Inlämning 1 ####
+
+# spara denna fil enligt:
+# del 1: inl1_josad732.R om ditt liu-ID är josad732 och det är inlämning 1 .
+
+
+# Skiv ditt namn här, ex Namn <- "Alice Walker"
+Namn <- ""
+
+
+# Skiv ditt liu ID här, ex LiuId <- "aliwa123"
+LiuId  <- ""
+
+
+# Uppgift 1 ----
+hi_name <- function(mitt_argument){
+  # Skriv din funktion här
+}
+
+# Uppgift 2 ----
+approx_e <- function(mitt_argument){
+  # Skriv din funktion här
+}
+
+# Uppgift 1 ----
+fast_stock_analysis <- function(mitt_argument){
+  # Skriv din funktion här
+}
+
+
+
+# OBS: kommentera bort all kod som inte är de obligatoriska variablerna ovan 
+# eller funktionerna som defineras i inlämningsuppgifterna!
+
+
+# Använda markmyassignment
+library(markmyassignment)
+set_assignment("https://raw.githubusercontent.com/STIMALiU/KursRprgm2/main/Labs/Tests/d1.yml")
+show_tasks()
+mark_my_assignment()
--- Labs/Tests/Tasks/hi_name_tests.R
+++ Labs/Tests/Tasks/hi_name_tests.R
@@ -13,15 +13,17 @@ test_that("Kontroll av hi_name", {
   expect_function_arguments(hi_name, "namn")
   
   expect_output(hi_name("Johan"), "Johan!",
-                info = "Fel: Funktionen skriver inte ut namnet.")
+                info = "Fel: Funktionen skriver inte ut namnet med utropstecken.")
   expect_output(hi_name("Lisa"), "Lisa!",
-                info = "Fel: Funktionen skriver inte ut namnet.")
+                info = "Fel: Funktionen skriver inte ut namnet med utropstecken.")
   expect_output(hi_name("Johan"), "Hi",
-                info = "Fel: Funktionen skriver inte ut Hi.")
+                info = "Fel: Funktionen skriver inte ut 'Hi' i början.")
   expect_output(hi_name("Johan"), "Keep coding!",
-                info = "Fel: Funktionen skriver inte ut 'I am your father'.")
+                info = "Fel: Funktionen skriver inte ut 'Keep coding!' i slutet.")
+  expect_output(hi_name("Josef"), "Hi Josef! Keep coding!",
+                info = "Fel: Funktionen skriver inte ut rätt textsträng, ska vara 'Hi Josef! Keep coding!'")
   temp <- capture.output(ret_val <- hi_name("Johan"))
   expect_null(ret_val,
-              info = "Fel: Funktionen ska inte returnera ngt, bara skriva ut till Console")
+              info = "Fel: Funktionen ska inte returnera något, bara skriva ut till Console")
 })
 
