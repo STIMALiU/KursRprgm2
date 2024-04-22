b8ba336f22baaeb04651d1c3cc6ac3e0bc6417b3
Johan Alenlöv <johan@alenlov.se>
2024-04-22 15:16:23 +0200

    Inl 4

---
+++ Labs/Templates/inl3_template.R
@@ -0,0 +1,38 @@
+#### Programmering i R ####
+#### Datorlaboration  [nummer] ####
+
+# spara denna fil enligt:
+# inl3_grupp01.R om ni tillhör grupp 01 och det är inl 3.
+
+
+# Skiv era namn här
+Namn <- c("namn1", "namn2")
+
+
+# Skiv era liu ID här
+LiuId  <- c("liuId1", "liuId2")
+
+# På del 2 i kursen, när ni arbetar i par, ange även grupptillhörighet enligt: 
+# Grupp<-"grupp01", om ni tillhör grupp01
+Grupp<-""
+
+
+
+# Uppgift 1 ----
+uppgift1 <- function(mitt_argument){
+  # Skriv din funktion här
+}
+
+
+
+
+
+# OBS: kommentera bort all kod som inte är de obligatoriska variablerna ovan 
+# eller funktionerna som defineras i inlämningsuppgifterna!
+
+
+# Använda markmyassignment
+library(markmyassignment)
+set_assignment("https://raw.githubusercontent.com/STIMALiU/KursRprgm2/main/Labs/Tests/inl3.yml")
+show_tasks()
+mark_my_assignment()
